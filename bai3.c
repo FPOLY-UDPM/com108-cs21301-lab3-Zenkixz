c77417cba66c45b723ae98fab749a6d91dbddda9
Zenkixz <senjixz@gmail.com>
2026-01-20 08:57:15 +0700

    com108-cs21301-lab3-AnhQuan

--- bai3.c
+++ bai3.c
@@ -13,23 +13,33 @@
 int main(){
     
     // Khai báo biến
-    int sodien;
+    int kWh;
 
     // Nhập dữ liệu
     printf("Nhap so dien tieu thu hang thang: ");
-    scanf("%d", &sodien);    
-
-     if (sodien <= 50) {
-        float tien = sodien * 1000;
+    scanf("%d", &kWh);      
+     if (kWh <= 50) {
+        float tien = kWh * 1.678;
         printf("So tien phai dong: %.2f VND\n", tien);
-    } else if (sodien <= 100) {
-        float tien = 50 * 1000 + (sodien - 50) * 1200;
+    } else if (kWh <= 100) {
+        float tien = 50 * 1.678 + (kWh - 50) * 1.734;
+        printf("So tien phai dong: %.2f VND\n", tien);
+    } else if (kWh > 100) {
+        float tien = 50 * 1.678 + 50 * 1.734 + (kWh - 100) * 2.014;
+        printf("So tien phai dong: %.2f VND\n", tien);
+    } else if (kWh > 200) {
+        float tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + (kWh - 200) * 2.536;
+        printf("So tien phai dong: %.2f VND\n", tien);
+    } else if (kWh > 300) {
+        float tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + (kWh - 300) * 2.834;
+        printf("So tien phai dong: %.2f VND\n", tien);
+    } else if (kWh > 400) {
+        float tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + 100 * 2.834 + (kWh - 400) * 2.927;
         printf("So tien phai dong: %.2f VND\n", tien);
     } else {
-        float tien = 50 * 1000 + 50 * 1200 + (sodien - 100) * 1500;
-        printf("So tien phai dong: %.2f VND\n", tien);
-    }
+        printf("So dien tieu thu khong hop le.\n");
+    }       
 
-    // Xử lý, tính toán VÀ Hiển thị kết quả
-    return 0;       
+    // Xử lý, tính toán VÀ Hiển thị kết quả 
+    return 0;             
 }   
